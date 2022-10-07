550b5a45d7960e249be4dc0f4314d894fcac3b67
Helio Cunha <helioncneto@gmail.com>
2022-10-07 18:43:52 -0300

    correction to evaluations using gpu

--- global_update_method/base_aggregation_IG.py
+++ global_update_method/base_aggregation_IG.py
@@ -8,7 +8,7 @@ from libs.methods.ig import selection_ig, update_participants_score, calc_ig
 from utils import get_scheduler, get_optimizer, get_model, get_dataset
 #import multiprocessing
 from torch import multiprocessing
-multiprocessing.set_start_method('spawn', force=True)
+multiprocessing.set_start_method('forkserver', force=True)
 import numpy as np
 from utils import *
 from libs.dataset.dataset_factory import NUM_CLASSES_LOOKUP_TABLE
