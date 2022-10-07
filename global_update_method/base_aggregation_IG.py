b258cf633119e4f1bbb1c110699c783f6590b829
Helio Cunha <helioncneto@gmail.com>
2022-10-07 17:27:30 -0300

    correction to evaluations using gpu

--- global_update_method/base_aggregation_IG.py
+++ global_update_method/base_aggregation_IG.py
@@ -220,7 +220,7 @@ def GlobalUpdate(args, device, trainset, testloader, local_update, valloader=Non
         # participant, model, global_weight, dataset, local_update, args, this_lr,  device, trainset,
         #this_alpha
         # Returns: participant, weight, loss, delta, num_of_data_clients
-        model.share_memory_()
+        model.share_memory()
         pack = Pack_Train(model=model, global_weight=global_weight, dataset=dataset, local_update=local_update,
                           args=args, this_lr=this_lr, device=device, trainset=trainset, this_alpha=this_alpha)
 
