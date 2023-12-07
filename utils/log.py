5185aa656ed05821613d0eba7ffec689cd87d6e6
Helio Cunha <helioncneto@gmail.com>
2023-12-07 20:05:04 -0300

    adding log system

--- utils/log.py
+++ utils/log.py
@@ -12,11 +12,12 @@ LOG_LEVEL = {'notset': logging.NOTSET,
 def setup_custom_logger(name: str, log_level: int, log_path: str):
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
-    handler = logging.StreamHandler()
+    # handler = logging.StreamHandler()
+    handler = FileHandler(filename=log_path, encoding='utf-8', level=log_level)
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
     logger.addHandler(handler)
-    logging.basicConfig(filename=log_path, encoding='utf-8', level=log_level)
+    # logging.basicConfig(filename=log_path, encoding='utf-8', level=log_level)
     return logger
