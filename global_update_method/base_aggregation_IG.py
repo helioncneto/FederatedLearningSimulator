39382f0d67923d283b742cb150eee0555d98d0e6
Helio Cunha <helioncneto@gmail.com>
2023-12-22 12:57:56 -0300

    avoiding data waste with malicious

--- global_update_method/base_aggregation_IG.py
+++ global_update_method/base_aggregation_IG.py
@@ -32,6 +32,7 @@ class FedSBSGlobalUpdate(BaseGlobalUpdate):
         self.eg_momentum = 0.9
         self.temperature = args.temperature
         self.cool = args.cool
+        print("CHEGUEI AQUI")
         print(self.participant_dataloader_table)
 
     def _get_dataloader_lookup_table(self):
